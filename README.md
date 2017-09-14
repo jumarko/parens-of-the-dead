79e67cbbcdd729cb148674e75fcf2322ab8844fc
Juraj Martinka <jumarko@gmail.com>
2017-09-14 10:08:58 +0200

    Project skeleton.

---
+++ README.md
@@ -0,0 +1,3 @@
+Parens of the Dead video series.
+
+See http://www.parens-of-the-dead.com/.
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject parens-of-the-dead "0.1.0-SNAPSHOT"
+  :description "A series of zombie-themed games written with Clojure and ClojureScript."
+  :url "http://www.parens-of-the-dead.com/"
+  :license {:name "General Public License"
+            :url "http://www.gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]]
+  :profiles {:dev {:plugins []
+                   :dependencies []
+                   :source-paths ["dev"]}}
+  )
